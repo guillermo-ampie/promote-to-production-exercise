02ed684f8fe7151bca2f2f849f02e184e366fd0d
Guillermo Ampie <ga.ampie@gmail.com>
2021-01-13 15:51:59 -0600

    Add quotes to variables

--- cloudfront-deploy.sh
+++ cloudfront-deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-S3_BUCKET_NAME=
-STACK_FILE=cloudfront.yml
-STACK_NAME=production-distro
-TAGS=project=udapeople
+S3_BUCKET_NAME="bucket-sample-001"
+STACK_FILE="cloudfront.yml"
+STACK_NAME="production-distro"
+TAGS="project=udapeople"
 
 echo "Deploying stack: ${STACK_NAME}..."
 
 aws cloudformation deploy \
 --template-file ${STACK_FILE} \
 --stack-name ${STACK_NAME} \
---parameter-overrides PipelineID="${S3_BUCKET_NAME}" \ 
+--parameter-overrides PipelineID=${S3_BUCKET_NAME} \
 --tags ${TAGS}
