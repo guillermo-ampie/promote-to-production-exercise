9557c110d18936350f94d7352c72ec1285dad56e
Guillermo Ampie <ga.ampie@gmail.com>
2021-01-15 12:01:17 -0600

    Update echo messages

--- build-first-deployment.sh
+++ build-first-deployment.sh
@@ -8,16 +8,16 @@ TAGS="project=udapeople"
 
 echo "Building first deployment..."
 
-echo "Creating bucket: ${S3_BUCKET_NAME}..."
+echo "Creating bucket: ${S3_BUCKET_NAME} ..."
 aws cloudformation deploy \
     --template-file bucket.yml \
     --stack-name "stack-${S3_BUCKET_NAME}" \
     --parameter-overrides BUCKETNAME="${S3_BUCKET_NAME}"
 
-echo "Syncing files to bucket: ${S3_BUCKET_NAME}..."
+echo "Syncing files to bucket: ${S3_BUCKET_NAME} ..."
 aws s3 sync . s3://"${S3_BUCKET_NAME}" --delete
 
-echo "Deploying stack: ${STACK_NAME}..."
+echo "Deploying stack: ${STACK_NAME} ..."
 aws cloudformation deploy \
     --template-file ${STACK_FILE} \
     --stack-name ${STACK_NAME} \
